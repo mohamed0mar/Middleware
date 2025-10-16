abe1498a53df889b108d12a7c165a2e7bf0b7c20
Mohamed Mahmoud <mohamedmahmoud.omar25@gmail.com>
2025-10-16 10:44:50 +0300

    Write Middleware Using app.Run

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -14,51 +14,68 @@ namespace Middleware
 
             var app = builder.Build();
 
-            //Pipeline And Middleware
-            ///Pipeline is a sequence of Middleware that process HTTP requests and responses [RequestDelegate]
-            ///Middleware is a software component that is used to handle requests and responses
-            ///Take RequestDelegate and process it and return new RequestDelegate
+            #region Pipeline && Middleware 
+            ////Pipeline And Middleware
+            /////Pipeline is a sequence of Middleware that process HTTP requests and responses [RequestDelegate]
+            /////Middleware is a software component that is used to handle requests and responses
+            /////Take RequestDelegate and process it and return new RequestDelegate
 
-            //First Middleware this the master middleware form 
-            //Take RequestDelegate and return RequestDelegate
-            //the next parameter is a RequestDelegate
-            app.Use((RequestDelegate next )=>next);
+            ////First Middleware this the master middleware form 
+            ////Take RequestDelegate and return RequestDelegate
+            ////the next parameter is a RequestDelegate
+            //app.Use((RequestDelegate next )=>next);
 
-            
-            //this middleware will write to the response and then call the next middleware
-            app.Use((RequestDelegate next) =>
+
+            ////this middleware will write to the response and then call the next middleware
+            //app.Use((RequestDelegate next) =>
+            //{
+            //    return (async (HttpContext context) =>
+            //    {
+            //        //write to the response
+            //        await context.Response.WriteAsync("Hello from Middleware 1\n");
+            //        await next(context); //call the next middleware
+            //    });
+            //});
+
+
+            ////this Extension method version of the above middleware
+            //app.Use(async (HttpContext context, RequestDelegate next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from Middleware 2\n");
+            //    await next(context); //call the next middleware
+            //});
+
+
+            ////this middlewar will not call the next middleware
+            //app.Use(async (HttpContext context, RequestDelegate next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from Middleware 3\n");
+            //    //await next(context);  dont call the next middleware
+            //});
+
+            ////this will not be called because the previous middleware did not call next
+            //app.Use(async (HttpContext context, RequestDelegate next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from Middleware 4\n");
+            //    await next(context); 
+            //}); 
+            #endregion
+
+
+
+            #region Write Middleware Using app.Run
+
+            //Adds a terminal middleware delegate to the application's request pipeline.
+            //this Middleware will be the last one in the pipeline
+            //This dont Call the next middleware
+            app.Run(async (HttpContext context) =>
             {
-                return (async (HttpContext context) =>
-                {
-                    //write to the response
-                    await context.Response.WriteAsync("Hello from Middleware 1\n");
-                    await next(context); //call the next middleware
-                });
+                await context.Response.WriteAsync("final middleware ");
+
             });
 
 
-            //this Extension method version of the above middleware
-            app.Use(async (HttpContext context, RequestDelegate next) =>
-            {
-                await context.Response.WriteAsync("Hello from Middleware 2\n");
-                await next(context); //call the next middleware
-            });
-
-
-            //this middlewar will not call the next middleware
-            app.Use(async (HttpContext context, RequestDelegate next) =>
-            {
-                await context.Response.WriteAsync("Hello from Middleware 3\n");
-                //await next(context);  dont call the next middleware
-            });
-
-            //this will not be called because the previous middleware did not call next
-            app.Use(async (HttpContext context, RequestDelegate next) =>
-            {
-                await context.Response.WriteAsync("Hello from Middleware 4\n");
-                await next(context); 
-            });
-
+            #endregion
 
 
             app.Run();
